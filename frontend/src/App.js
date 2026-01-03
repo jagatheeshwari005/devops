e24fba61b42a30464e45e12e01e0582615967804
jagatheeshwari005 <jagatheeshwaril.23it@kongu.edu>
2026-01-03 21:11:57 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import AddProduct from "./components/AddProduct";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div>
+      <h2>Electrical Shop Inventory</h2>
+      <AddProduct />
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
